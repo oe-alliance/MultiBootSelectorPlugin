36a259eed4f4592acd315181503c813c23164afb
WXbet <57314510+WXbet@users.noreply.github.com>
2025-07-23 17:56:26 +0200

    improve button layout
    - use ePixmap for button background
    - align the button texts vertically centered
    - add 10px of space to the bottom of the screen
    - reload button images for better compatibility

--- src/plugin.py
+++ src/plugin.py
@@ -13,9 +13,11 @@ from Screens.Console import Console
 from Plugins.Plugin import PluginDescriptor
 from Components.MenuList import MenuList
 from Components.ActionMap import ActionMap
+from Components.Pixmap import Pixmap
 from Components.Button import Button
 from Components.Label import Label
 from Screens.Standby import TryQuitMainloop
+from Tools.Directories import fileExists, resolveFilename, SCOPE_SKIN_IMAGE
 
 try:
 	from Components.SystemInfo import BoxInfo
@@ -47,13 +49,21 @@ def main(session, **kwargs):
 
 class Scripts(Screen):
 	skin = """
-	<screen position="center,center" size="660,500" title="MultiBoot Selector">
+	<screen position="center,center" size="660,520" title="MultiBoot Selector">
 		<widget name="header" position="10,0" size="640,30" font="Regular;22" halign="center" />
 		<widget name="list" position="10,35" size="640,415" scrollbarMode="showOnDemand" />
-		<widget name="key_red" position="10,470" size="150,30" backgroundColor="red" foregroundColor="white" font="Regular;20" halign="center" />
-		<widget name="key_green" position="170,470" size="150,30" backgroundColor="green" foregroundColor="white" font="Regular;20" halign="center" />
-		<widget name="key_yellow" position="330,470" size="150,30" backgroundColor="yellow" foregroundColor="white" font="Regular;20" halign="center" />
-		<widget name="key_blue" position="490,470" size="150,30" backgroundColor="blue" foregroundColor="white" font="Regular;20" halign="center" />
+
+		<widget name="key_red_pixmap" pixmap="skin_default/buttons/red.png" position="10,470" size="150,40" scale="stretch" alphatest="on" />
+		<widget name="key_red" position="10,470" size="150,40" font="Regular;20" zPosition="1" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" shadowColor="black" shadowOffset="-2,-2" />
+
+		<widget name="key_green_pixmap" pixmap="skin_default/buttons/green.png" position="170,470" size="150,40" scale="stretch" alphatest="on" />
+		<widget name="key_green" position="170,470" size="150,40" font="Regular;20" zPosition="1" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" shadowColor="black" shadowOffset="-2,-2" />
+
+		<widget name="key_yellow_pixmap" pixmap="skin_default/buttons/yellow.png" position="330,470" size="150,40" scale="stretch" alphatest="on" />
+		<widget name="key_yellow" position="330,470" size="150,40" font="Regular;20" zPosition="1" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" shadowColor="black" shadowOffset="-2,-2" />
+
+		<widget name="key_blue_pixmap" pixmap="skin_default/buttons/blue.png" position="330,470" size="150,40" scale="stretch" alphatest="on" />
+		<widget name="key_blue" position="490,470" size="150,40" font="Regular;20" zPosition="1" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" shadowColor="black" shadowOffset="-2,-2" />
 	</screen>"""
 
 	def __init__(self, session, args=None):
@@ -67,18 +77,22 @@ class Scripts(Screen):
 		self.output_lines = []
 		self.reload_list()
 		self["list"] = MenuList([item[1] for item in self.slist])
-		self["list"].l.setItemHeight(35)
+		self["list"].l.setItemHeight(30)
 
-		# colored buttons labels
-		self["key_red"] = Label(_("Cancel"))
-		self["key_green"] = Label(_("Restart"))
-		self["key_yellow"] = Label(_("Reset"))
-		self["key_blue"] = Label(_("tbd"))
+		self["key_red"] = Button(_("Cancel"))
+		self["key_red_pixmap"] = Pixmap()
+		self["key_green"] = Button(_("Restart"))
+		self["key_green_pixmap"] = Pixmap()
+		self["key_yellow"] = Button(_("Reset"))
+		self["key_yellow_pixmap"] = Pixmap()
+		self["key_blue"] = Button(_("tbd"))
 		self["key_blue"].hide()
+		self["key_blue_pixmap"] = Pixmap()
+		self["key_blue_pixmap"].hide()
 
 		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"], {
-			"ok": self.run,
-			"cancel": self.close,
+			"ok": self.greenPressed,
+			"cancel": self.redPressed,
 			"red": self.redPressed,
 			"green": self.greenPressed,
 			"yellow": self.yellowPressed,
@@ -87,7 +101,7 @@ class Scripts(Screen):
 
 		self.updateButtons()
 		self["list"].onSelectionChanged.append(self.updateButtons)
-		self.onLayoutFinish.append(self.selectCurrentLine)
+		self.onLayoutFinish.append(self.onLayoutFinished)
 
 
 	def run(self):
@@ -108,7 +122,6 @@ class Scripts(Screen):
 			if not isfile(self.cmd):
 				self.slist = [[-1, "Error: File '%s' is not available!" % self.cmd]]
 			else:
-				# call cmd process
 				process = Popen([self.cmd, "list"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
 				stdout, stderr = process.communicate()
 
@@ -130,17 +143,35 @@ class Scripts(Screen):
 			self.slist = [[-1, "Error: %s" % e]]
 
 
-	def selectCurrentLine(self):
-		self["list"].moveToIndex(self.currentIndex)
+	def onLayoutFinished(self):
+		try:
+			# select current running image in the list
+			self["list"].moveToIndex(self.currentIndex)
+			# reload button images
+			reloadButton("red")
+			reloadButton("green")
+			reloadButton("yellow")
+			reloadButton("blue")
+		except:
+			pass
+
+
+	def reloadButton(self, color):
+		image_png = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/buttons/%s.png" % color)
+		if fileExists(image_png): self["key_%s_pixmap" % color].instance.setPixmapFromFile(image_png)
+		image_svg = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/buttons/%s.svg" % color)
+		if fileExists(image_svg): self["key_%s_pixmap" % color].instance.setPixmapFromFile(image_svg)
+		return
 
 
 	def updateButtons(self):
-		"""Hide green button completely if current entry contains 'Empty'."""
 		current = self["list"].getCurrent()
 		if current and "Empty" in current:
 			self["key_green"].hide()
+			self["key_green_pixmap"].hide()
 		else:
 			self["key_green"].show()
+			self["key_green_pixmap"].show()
 
 
 	def redPressed(self):
